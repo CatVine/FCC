8c370abc9c4f6c889dcf436236964f8bcceb987c
Cat Vine <catriona.vine@stormid.com>
2023-02-07 19:58:57 +0000

    json

--- myApp.js
+++ myApp.js
@@ -1,7 +1,23 @@
+require('dotenv').config()
+
 let express = require('express');
 let app = express();
 
+app.get("/", function(req, res) {
+//   res.send('Hello Express');
+  res.sendFile(__dirname + "/views/index.html")
+})
 
+app.use("/public", express.static(__dirname + "/public"))
+
+app.get("/json", function(req, res) {
+    const style = process.env.MESSAGE_STYLE;
+    if (style == "uppercase") {
+        res.json({"message": "HELLO JSON"})
+    } else {
+    res.json({"message": "Hello json"})
+    }
+})
 
 
 
